5015b091e0905f2b394a2ac841020db6917391a1
hisataka0131 <biy0ganba@gmail.com>
2021-07-01 14:08:11 +0000

    bootstrap

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destory
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_113308) do
+ActiveRecord::Schema.define(version: 2021_07_01_134645) do
 
   create_table "books", force: :cascade do |t|
     t.string "book_title"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_07_01_113308) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
